47c711fae63773d61afd51a20ae7e21751935b86
Gatilin Maxim <gatilin222@gmail.com>
2017-09-23 11:43:42 +0300

    Create new variable

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	console.log('Test for super branch');
+	const a = 'String for a variable';
 	ReactDOM.render(<App />, div);
 });
