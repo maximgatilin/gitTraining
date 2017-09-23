8f05b5f3c0d819144bdcf01cbbc353b42e6ffa1f
Gatilin Maxim <gatilin222@gmail.com>
2017-09-23 11:44:58 +0300

    Add b variable

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	console.log('Test for super branch');
+	const b = 12;
 	ReactDOM.render(<App />, div);
 });
