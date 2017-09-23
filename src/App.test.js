08a0dca8a36f980abd3a4cfa5d762b0929909d72
Gatilin Maxim <gatilin222@gmail.com>
2017-09-23 17:29:50 +0300

    Switch b to 18

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,6 @@ import App from './App';
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	const a = 'String for a variable';
-    const b = 12;
+    const b = 18;
 	ReactDOM.render(<App />, div);
 });
