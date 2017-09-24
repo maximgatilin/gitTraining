40de22911f6fc2acc4abf8a8bd9d9163e6f98e47
Gatilin Maxim <gatilin222@gmail.com>
2017-09-24 12:51:31 +0300

    Add another className to footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 			<div className={styles.container}>
 				<Header />
 				<MainContainer />
-				<Footer className="exp branch"/>
+				<Footer className="another" />
 			</div>
 		);
 	}
